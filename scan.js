16700cba627541d944a0f437c586b67bdb859993
Buck Baskin <mobile.wbaskin@gmail.com>
2017-05-01 05:30:31 +0000

    Add functionality to the scan file

--- scan.js
+++ scan.js
@@ -2,7 +2,7 @@ var noble = require('noble');
 
 let activeScanning = false;
 
-const activeStateString = 'poweredO';
+const activeStateString = 'poweredOn';
 
 console.log('setup noble');
 
@@ -15,14 +15,34 @@ noble.on('stateChange', (state) => {
         throw err;
       }
     }); //  any service UUID, no duplicates
-
-    noble.stopScanning();
   } else {
     console.log('state is not', activeStateString);
     console.log('state is', state);
   }
 });
 
+noble.on('discover', (peripheral) => {
+  console.log('1. noble device found');
+  console.log('1. device id', peripheral.id, 'addr', peripheral.addr);
+  console.log('1. observed rssi', peripheral.rssi, 'from advert', peripheral.advertisement.localName, peripheral.advertisement.txPowerLevel);
+  peripheral.once('rssiUpdate', (err, rssi) => {
+    console.log('2. device id', peripheral.id, 'addr', peripheral.addr);
+    console.log('2. observed rssi', rssi, 'from advert', peripheral.advertisement.localName, peripheral.advertisement.txPowerLevel);
+  });
+  peripheral.once('connect', (arg) => {
+    console.log('peripheral connected', arg);
+  });
+  peripheral.once('disconnect', (arg) => {
+    console.log('peripheral disconnected', arg);
+  });
+});
+
 noble.on('warning', (message) => {
   console.log('noble warning', message);
+});
+
+noble.on('error', (err) => {
+  console.log('noble error', err);
+  noble.stopScanning();
+  throw err;
 });
