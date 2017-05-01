62fe5d23bbc0d9f0b1ace6820b71d82ce42a5c13
Buck Baskin <buckbaskin@google.com>
2017-05-01 00:39:35 -0400

    Try some basic functionality in nodejs

--- scan.js
+++ scan.js
@@ -1,1 +1,28 @@
-console.log("scan.js: There are no bluetooth devices found yet.");
+var noble = require('noble');
+
+let activeScanning = false;
+
+const activeStateString = 'poweredO';
+
+console.log('setup noble');
+
+noble.on('stateChange', (state) => {
+  if (state == activeStateString) {
+    console.log('state is', activeStateString);
+    noble.startScanning([], false, (err) => {
+      if (err) {
+        console.log(err.what());
+        throw err;
+      }
+    }); //  any service UUID, no duplicates
+
+    noble.stopScanning();
+  } else {
+    console.log('state is not', activeStateString);
+    console.log('state is', state);
+  }
+});
+
+noble.on('warning', (message) => {
+  console.log('noble warning', message);
+});
